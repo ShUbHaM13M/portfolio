760fab05deb3b313032a4aebc2cc61c45f0a97e1
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-01-25 18:16:32 +0530

    Added BlogCard Component

---
+++ src/lib/components/molecules/BlogPostCard.story.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import BlogPostCard from './BlogPostCard.svelte';
+
+	export let Hst: Hst;
+</script>
+
+<Hst.Story title="Molecules/Blog Post Card" layout={{ type: 'grid', width: 400 }}>
+	<div style="padding: 12px;">
+		<Hst.Variant title="Default">
+			<BlogPostCard
+				slug="blog-post"
+				title="Blog Post"
+				excerpt="This is a blog post"
+				imageUrl="https://cdn.sanity.io/images/bj34pdbp/migration/bca4bfbc722f4acaceaece20a0ef957e9f18bb6e-2024x1012.png?w=1920&q=75&fit=clip&auto=format"
+				date="Nov 25, 2024"
+			/>
+		</Hst.Variant>
+
+		<Hst.Variant title="No Image">
+			<BlogPostCard
+				slug="blog-post"
+				title="Blog Post"
+				excerpt="This is a blog post"
+				date="Nov 25, 2024"
+			/>
+		</Hst.Variant>
+	</div>
+</Hst.Story>
---
+++ src/lib/components/molecules/BlogPostCard.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let slug: string;
+	export let imageUrl: string | undefined = undefined;
+	export let date: string;
+	export let title: string;
+	export let excerpt: string;
+</script>
+
+<a href={slug}>
+	<div
+		style="max-width: 350px"
+		class="p-4 py-5 flex flex-col gap-4 border border-text dark:border-text-dark"
+	>
+		{#if imageUrl}
+			<img
+				style="max-width: 350px"
+				class="max-w-full w-full object-cover aspect-video rounded-2xl"
+				src={imageUrl}
+				alt={title}
+			/>
+		{/if}
+		<span class="opacity-40 text-sm">{date}</span>
+		<h3 class="text-3xl font-bold">{title}</h3>
+		<p class="text-xl">{excerpt}</p>
+	</div>
+</a>
