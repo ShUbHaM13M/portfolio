20fda7650ac7fd38cd5fb40c88b7c50368813438
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-01-28 19:33:06 +0530

    Added Button component

---
+++ src/lib/components/atoms/Button.story.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import Button, { type Variant } from './Button.svelte';
+
+	export let Hst: Hst;
+
+	let props: {
+		variant: Variant;
+		href: string;
+		target: '_blank';
+	} = {
+		variant: 'outlined',
+		href: '',
+		target: '_blank'
+	};
+
+	let text = 'This is a button';
+</script>
+
+<svelte:component this={Hst.Story} title="Atoms/Button" layout={{ type: 'grid', width: 400 }}>
+	<svelte:fragment slot="controls">
+		<Hst.Text bind:value={text} title="Text" />
+		<Hst.Select bind:value={props.variant} title="Variant" options={['filled', 'outlined']} />
+		<Hst.Text bind:value={props.href} title="Href" />
+	</svelte:fragment>
+	<div style="padding: 12px;">
+		<svelte:component this={Hst.Variant} title="No Icon">
+			<Button {...props}>
+				{text}
+			</Button>
+		</svelte:component>
+	</div>
+</svelte:component>
---
+++ src/lib/components/atoms/Button.svelte
@@ -0,0 +1,46 @@
+<script context="module" lang="ts">
+	export type Variant = 'filled' | 'outlined';
+</script>
+
+<script lang="ts">
+	export let variant: Variant = 'outlined';
+	export let additionalClass: string | undefined = undefined;
+	export let href: string | undefined = undefined;
+	export let target: '' | '_blank' = '';
+	// TODO: Update based on external link
+	export let rel = 'noopener noreferrer';
+	$: tag = href ? 'a' : 'button';
+	$: linkProps = {
+		href,
+		target,
+		rel
+	};
+</script>
+
+<svelte:element
+	this={tag}
+	{...linkProps}
+	role="button"
+	tabindex="0"
+	data-sveltekit-preload-data
+	on:click
+	class={`${variant} px-4 w-fit py-3 mt-6 font-medium text-xl rounded-md 	transition-colors ease-out duration-300 ${additionalClass}`}
+	{...$$restProps}
+>
+	{#if $$slots['icon']}
+		<div class="icon">
+			<slot name="icon" />
+		</div>
+	{/if}
+	<slot />
+</svelte:element>
+
+<style lang="postcss" scoped>
+	.filled {
+		@apply bg-accent text-primary;
+	}
+	.outlined {
+		outline: 1px solid theme(colors.accent);
+		@apply text-accent hover:bg-accent focus-visible:bg-accent hover:text-primary focus-visible:text-primary;
+	}
+</style>
