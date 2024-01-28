530e962513d2ea28628f1334527f0c3e385c9d69
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-01-28 19:32:32 +0530

    Updated tailwind vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,31 +4,11 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				text: {
-					light: '#041C03',
-					DEFAULT: '#041C03',
-					dark: '#E4FCE3'
-				},
-				background: {
-					light: '#F8F6F4',
-					DEFAULT: '#F8F6F4',
-					dark: '#020D02'
-				},
-				primary: {
-					light: '#41E92B',
-					DEFAULT: '#41E92B',
-					dark: '#2CD416'
-				},
-				secondary: {
-					light: '#A2D7C9',
-					DEFAULT: '#A2D7C9',
-					dark: '#285D4F'
-				},
-				accent: {
-					light: '#2CC9BE',
-					DEFAULT: '#2CC9BE',
-					dark: '#36D3C8'
-				}
+				primary: '#0B192F',
+				subtitle: '#8892AF',
+				secondary: '#CDD1DA',
+				white: '#F8EDFF',
+				accent: '#19D645'
 			}
 		}
 	},
