dd5964225ff24971df8f58be65111b3d17041e9b
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-01-28 19:33:14 +0530

    Added Logo component

---
+++ src/lib/components/atoms/Logo.story.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import Logo from './Logo.svelte';
+
+	export let Hst: Hst;
+</script>
+
+<Hst.Story title="Atoms/Logo" layout={{ type: 'single' }}>
+	<Logo />
+</Hst.Story>
