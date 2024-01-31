6cfb2fb43f67fb800bc1c49af84181c243670e49
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-01-31 21:39:10 +0530

    Updated project page copy

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,18 +2,8 @@
 	class="flex flex-col fixed top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 text-white gap-6"
 >
 	<h1 class="text-4xl">Not Yet Designed!</h1>
-	<p class="mt-3 relative text-lg text-center">Made with 🧠 by Shubham</p>
+	<p class="mt-3 relative text-lg text-center">
+		Made with 🧠 by Shubham
+		<span class="text-accent">And Deva's ✨</span>
+	</p>
 </div>
-
-<style lang="postcss">
-	p::after {
-		content: "And Deva's ✨";
-		position: absolute;
-		color: #19d645;
-		top: -2px;
-		right: 0%;
-		transform-origin: left;
-		transform: translateX(50%) rotateZ(-12deg);
-		display: block;
-	}
-</style>
