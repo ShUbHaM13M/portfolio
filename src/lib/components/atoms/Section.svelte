08d11d83d344daba57efa354a7ad916d57fb550a
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-01-31 21:22:38 +0530

    Added Section component

---
+++ src/lib/components/atoms/Section.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let additionalClass: string = '';
+</script>
+
+<section {...$$restProps} class={`px-6 md:pl-20 pt-10 min-h-screen ${additionalClass}`}>
+	<slot />
+</section>
