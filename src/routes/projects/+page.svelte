0fa4680cb97b0922b56fe078f6c93540d0f9e015
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-01-31 21:49:05 +0530

    update text alignment

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,5 +1,5 @@
 <div
-	class="flex flex-col fixed top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 text-white gap-6"
+	class="flex flex-col fixed top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 text-white gap-6 w-full text-center"
 >
 	<h1 class="text-4xl">Not Yet Designed!</h1>
 	<p class="mt-3 relative text-lg text-center">
