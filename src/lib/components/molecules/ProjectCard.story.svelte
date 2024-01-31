07efbf3e7a34914c2bc711d6143f0238efc86415
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-01-31 21:22:56 +0530

    Added ProjectCard component

---
+++ src/lib/components/molecules/ProjectCard.story.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import ProjectCard from './ProjectCard.svelte';
+	import IconSoftwareDevelopement from '$lib/icons/IconSoftwareDevelopement.svelte';
+
+	export let Hst: Hst;
+
+	let props = {
+		title: 'Stories.io - Story App',
+		excerpt:
+			"Stories.io is a React Native app that connect with an expressjs server, allows users to browse, create, change and delete their stories stores on a mongoDb database. Users can like and comments on other user's stories and also set their stories as private"
+	};
+
+	let imageUrl =
+		'https://cdn.sanity.io/images/bj34pdbp/migration/bca4bfbc722f4acaceaece20a0ef957e9f18bb6e-2024x1012.png?w=1920&q=75&fit=clip&auto=format';
+</script>
+
+<Hst.Story title="Molecules/Project Card" layout={{ type: 'grid', width: 400 }}>
+	<svelte:fragment slot="controls">
+		<Hst.Text bind:value={props.title} title="Title" />
+		<Hst.Textarea bind:value={props.excerpt} title="Excerpt" />
+		<Hst.Text bind:value={imageUrl} title="Image URL" />
+	</svelte:fragment>
+
+	<div style="padding: 12px;">
+		<Hst.Variant title="Default">
+			<ProjectCard
+				links={[
+					{
+						href: 'https://www.github.com',
+						icon: IconSoftwareDevelopement
+					}
+				]}
+				{...props}
+				{imageUrl}
+			/>
+		</Hst.Variant>
+
+		<Hst.Variant title="No Image">
+			<ProjectCard {...props} />
+		</Hst.Variant>
+	</div>
+</Hst.Story>
---
+++ src/lib/components/molecules/ProjectCard.svelte
@@ -0,0 +1,70 @@
+<script lang="ts" context="module">
+	import type { SvelteComponent } from 'svelte';
+	export type Link = {
+		href: string;
+		icon: typeof SvelteComponent;
+	};
+
+	export type Tag = {
+		text: string;
+		backgroundColor?: string;
+		color: string;
+	};
+</script>
+
+<script lang="ts">
+	export let imageUrl: string | undefined = undefined;
+	export let title: string = '';
+	export let excerpt: string = '';
+	export let link: Link | undefined = undefined;
+
+	export let containerClasses: string | undefined = undefined;
+	export let tags: Tag[] = [];
+</script>
+
+<div
+	class={`container flex flex-col gap-2 md:gap-4 p-4 md:p-6 text-white rounded-lg bg-header ${containerClasses}`}
+>
+	{#if imageUrl}
+		<img
+			class="project-card-image aspect-video w-full rounded-md"
+			src={imageUrl}
+			alt={`Screenshot of ${title}`}
+		/>
+	{/if}
+	<div class="flex justify-between items-center">
+		<p class="font-medium text-2xl sm:text-2xl leading-8">{title}</p>
+		{#if link}
+			<a href={link.href} target="_blank" title="">
+				<svelte:component this={link.icon} />
+			</a>
+		{/if}
+	</div>
+	<p style="--number-of-lines: 2" class="text-base sm:text-xl leading-5 sm:leading-6 clamp-lines">
+		{excerpt}
+	</p>
+	<div class="flex flex-wrap gap-2">
+		<!-- TODO: Add textColor when backgroundColor is not provided    -->
+		{#each tags.slice(0, 3) as tag}
+			<div
+				style={`
+					color: ${tag.color};
+					background-color: ${tag.backgroundColor};
+				`}
+				class="grid place-items-center text-sm md:text-base py-1 md:py-2 px-4 md:px-6 rounded-full capitalize font-bold"
+			>
+				{tag.text}
+			</div>
+		{/each}
+	</div>
+</div>
+
+<style lang="postcss" scoped>
+	.container {
+		max-width: 400px;
+	}
+	.project-card-image {
+		max-height: 140px;
+		@apply object-cover;
+	}
+</style>
