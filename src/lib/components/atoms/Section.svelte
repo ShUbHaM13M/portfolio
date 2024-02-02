d024f539feadb0d708f8313557fc0e119ddc7996
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-02 15:50:37 +0530

    Updated section component to take props

--- src/lib/components/atoms/Section.svelte
+++ src/lib/components/atoms/Section.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
 	export let additionalClass: string = '';
+	export let title: string = '';
+	export let description: string = '';
 </script>
 
 <section {...$$restProps} class={`px-6 md:pl-20 pt-10 min-h-screen ${additionalClass}`}>
+	<h1 class="font-short-stack mt-2 text-white text-4xl sm:text-6xl">{title}</h1>
+	{#if $$slots.description}
+		<slot name="description" />
+	{:else}
+		<h2 class="leading-6 sm:leading-10 text-subtitle text-2xl">
+			{description}
+		</h2>
+	{/if}
 	<slot />
 </section>
