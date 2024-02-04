f0fb5f31046c4f0c89ab61822470f5bf15bad7c7
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-04 12:36:31 +0530

    Replace Blogs with RecentBlogs

---
+++ src/lib/components/organism/RecentBlogs.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import Button from '../atoms/Button.svelte';
+	import type { Blog } from '$lib/utils/types';
+	import BlogPostCard from '../molecules/BlogPostCard.svelte';
+	import Section from '../atoms/Section.svelte';
+	export let blogs: Blog[] = [];
+</script>
+
+<Section
+	id="blog"
+	title="Recent Blogs"
+	additionalClass="flex flex-col px-6 pb-10 md:px-20 pt-10 min-h-screen"
+>
+	<slot name="description">
+		<h2 class="leading-6 sm:leading-tight text-subtitle text-lg md:text-2xl mt-2">
+			Every now and then I like to write about technology and experiences. <br /> Here are some of the
+			recent blogs.
+		</h2>
+	</slot>
+	<div class="flex flex-col flex-wrap md:flex-row gap-4 mt-6 md:mt-8">
+		{#each blogs as blog}
+			<BlogPostCard {...blog} />
+		{/each}
+	</div>
+	<Button
+		additionalClass="mt-4 w-full text-center md:w-fit md:mt-8 md:ml-auto"
+		href="/blog"
+		variant="outlined"
+	>
+		View More
+	</Button>
+</Section>
