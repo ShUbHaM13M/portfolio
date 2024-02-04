12ae976de05781d6c60eaccbb2710447b3ab77a3
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-04 21:04:09 +0530

    Fix unable to build due to missing HttpError handler

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,12 @@ const config = {
 
 	kit: {
 		prerender: {
-			handleMissingId: 'ignore'
+			handleMissingId: 'ignore',
+			handleHttpError: ({ path }) => {
+				if (path.indexOf('.avif') || path.indexOf('.webp') || path.indexOf('.png')) {
+					return;
+				}
+			}
 		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
