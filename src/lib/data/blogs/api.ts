e8535522ce4d9a2d4972ccc3249fb9814c6fc765
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-04 12:37:37 +0530

    Added Api for parsing blogs from md files

--- src/lib/data/blogs/api.ts
+++ src/lib/data/blogs/api.ts
@@ -1,23 +1,50 @@
 import fs from 'fs-extra';
 import grayMatter from 'gray-matter';
 import path from 'path';
-import { parseSkills } from "./model";
-import type { Blog } from '$lib/utils/types';
+import { parseBlogs } from "./model";
+import type { Blog, PaginatedResult } from '$lib/utils/types';
 
+const MAX_BLOGS_PER_PAGE = 12;
 const MD_FILES_PATH = path.join(process.cwd(), 'cms/blogs');
 
-export const getBlogs = async (): Promise<Blog[]> => {
+export const getBlogs = async (page?: number, count?: number, showHidden = false): Promise<PaginatedResult<Blog>> => {
 	const fileNames = await fs.readdir(MD_FILES_PATH);
-	const mdFiles = fileNames.filter((fileName: string) => fileName.endsWith('.md'));
+	let mdFiles = fileNames.filter((fileName: string) => fileName.endsWith('.md'));
 	mdFiles.reverse();
 
-	const items: Blog[] = [];
-
-	for (const mdFile of mdFiles) {
-		const fileContent = await fs.readFile(`${ MD_FILES_PATH }/${ mdFile }`, 'utf-8');
-		const parsedData = grayMatter(fileContent);
-		items.push(parseSkills(parsedData.data));
+	if (!showHidden) {
+		mdFiles = mdFiles.filter(item => !item.endsWith('--hidden.md'))
 	}
 
-	return items;
+	const paginatedBlogs = paginate(mdFiles, page, count);
+
+	const blogs: Blog[] = [];
+
+	for (const mdFile of paginatedBlogs) {
+		const fileContent = await fs.readFile(`${ MD_FILES_PATH }/${ mdFile }`, 'utf-8');
+		const parsedData = grayMatter(fileContent);
+		blogs.push(parseBlogs(parsedData.data));
+	}
+
+	return {
+		items: blogs,
+		totalItems: mdFiles.length,
+		totalPages: Math.ceil(mdFiles.length / (count ?? MAX_BLOGS_PER_PAGE)),
+		currentPage: page ?? 1,
+	};
+}
+
+export const getBlogBySlug = async (slug: string): Promise<Blog | null> => {
+	const allBlogs = await getBlogs(undefined, undefined, true);
+	const matchedBlog = allBlogs.items?.find((blog) => blog.slug === slug) ?? null
+	// TODO: make a list of related blogs
+	return matchedBlog
+}
+
+const paginate = (items: string[], page?: number, count?: number): string[] => {
+	const pageSize = count ?? MAX_BLOGS_PER_PAGE;
+	const startIndex = page ? (page - 1) * pageSize : 0;
+	const endIndex = page ? page * pageSize : count ?? 9999;
+	const paginatedItems = items.slice(startIndex, endIndex)
+	return paginatedItems
 }
--- src/lib/data/blogs/model.ts
+++ src/lib/data/blogs/model.ts
@@ -1,10 +1,11 @@
 import type { Blog } from "$lib/utils/types";
 
-export function parseSkills(frontmatter: any): Blog {
+export function parseBlogs(frontmatter: any): Blog {
 	return {
 		slug: frontmatter.slug,
-		title: frontmatter.slug,
+		title: frontmatter.title,
 		coverImage: frontmatter.coverImage,
+		socialImage: frontmatter.socialImage,
 		date: frontmatter.date,
 		excerpt: frontmatter.excerpt,
 		hidden: frontmatter.hidden,
