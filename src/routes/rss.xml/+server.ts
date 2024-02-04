c45918f6258e434ec4c1b2595b49574312666987
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-04 12:43:35 +0530

    Added GET function so that it builds on vercel

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -1,1 +1,9 @@
 export const prerender = true;
+
+export async function GET() {
+	const headers = {
+		'Cache-Control': 'max-age=0, s-maxage=3600',
+		'Content-Type': 'application/xml'
+	}
+	return new Response("Body", { headers })
+}
