cc9c85dab5c546ef32ca32f5823fe095de9cdc67
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-06 22:16:20 +0530

    Added Error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Button from '$lib/components/atoms/Button.svelte';
+	import Header from '$lib/components/organism/Header.svelte';
+</script>
+
+<Header />
+
+<main>
+	<div>
+		<h1>Error Occured</h1>
+		<p>Seems like some unknown error happened</p>
+		<Button href="/">Let's start over</Button>
+	</div>
+</main>
