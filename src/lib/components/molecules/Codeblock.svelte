3e430a3bda6fc7dd3f63b1afb7f2de0e78fa8841
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-08 23:48:13 +0530

    Added copy function on codeblock

--- src/lib/components/molecules/Codeblock.svelte
+++ src/lib/components/molecules/Codeblock.svelte
@@ -17,10 +17,15 @@
 	export let filename: string | undefined = undefined;
 	export let lang: string;
 	export let code: string | undefined = undefined;
+
+	function onCopyClick() {
+		// TODO: Indicator when copied to clipboard
+		if (code) navigator.clipboard.writeText(code);
+	}
 </script>
 
-<div class="flex flex-col rounded-xl bg-primary">
-	<div class="flex justify-between py-2 rounded-t-xl px-4 md:px-6 items-center bg-[#0B192F]">
+<div class="flex flex-col rounded-xl bg-[#12223A]">
+	<div class="flex justify-between py-2 rounded-t-xl px-4 md:px-6 items-center bg-primary">
 		<div class="flex items-center gap-2.5">
 			<div class="flex gap-1 md:flex">
 				<div class="w-3 h-3 rounded-full bg-red-400"></div>
@@ -34,11 +39,11 @@
 				<span class="capitalize">{lang}</span>
 			</div>
 		</div>
-		<button>
+		<button on:click={onCopyClick}>
 			<IconCopy />
 		</button>
 	</div>
-	<div class="p-4 md:p-6 text-base md:text-xl">
+	<div class="code-block p-4 md:p-6 text-base md:text-xl">
 		{#if code}
 			{#if lang}
 				<pre
