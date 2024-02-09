59a75fd68e1415159a964c59aaf0f68ca7c9a11c
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-09 20:17:31 +0530

    Fix entries not being generated

--- src/routes/(blog)/[slug]/+page.server.ts
+++ src/routes/(blog)/[slug]/+page.server.ts
@@ -1,11 +1,14 @@
 import { getAllSlugs } from '$lib/data/blogs/api';
+import { EntryGenerator } from './$types.js';
 
 export async function load({ parent }) {
 	const data = await parent();
 	return data;
 }
 
-export async function entries() {
+export const entries: EntryGenerator = async () => {
 	const slugs = await getAllSlugs();
 	return slugs.map((slug) => ({ slug }));
-}
+};
+
+export const prerender = true;
