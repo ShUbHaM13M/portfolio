49abe266027bfc30b867ef4bbdadbedbb7153d44
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-10 17:21:59 +0530

    Added custom renderers in md

--- src/lib/components/molecules/Markdown.svelte
+++ src/lib/components/molecules/Markdown.svelte
@@ -2,12 +2,29 @@
 	import { marked } from 'marked';
 	import SvelteMarkdown from 'svelte-markdown';
 	import ComponentRenderer from '../markdown/ComponentRenderer.svelte';
+	import HeadingRenderer from '../markdown/HeadingRenderer.svelte';
+	import ImageRenderer from '../markdown/ImageRenderer.svelte';
+	import HtmlRenderer from '../markdown/HtmlRenderer.svelte';
+	import HrRenderer from '../markdown/HrRenderer.svelte';
+	import BlockQuoteRenderer from '../markdown/BlockQuoteRenderer.svelte';
+	import LinkRenderer from '../markdown/LinkRenderer.svelte';
+	import ListRenderer from '../markdown/ListRenderer.svelte';
 
 	export let content: string = '';
-	const options = marked.defaults;
+	const options = {
+		...marked.defaults,
+		mangle: false
+	};
 
 	const renderers = {
-		code: ComponentRenderer
+		code: ComponentRenderer,
+		heading: HeadingRenderer,
+		image: ImageRenderer,
+		html: HtmlRenderer,
+		hr: HrRenderer,
+		blockquote: BlockQuoteRenderer,
+		link: LinkRenderer,
+		list: ListRenderer
 	} as any;
 </script>
 
