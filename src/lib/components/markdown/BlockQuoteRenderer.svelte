d48a0cd512aae36b548836644738afa8cd55fe36
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-10 17:20:28 +0530

    Added BlockQuote Renderer

---
+++ src/lib/components/markdown/BlockQuoteRenderer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let raw: string;
+	export let text: string;
+
+	let lines = raw.split('>').slice(1);
+	lines = lines.map((line) => line.trim());
+</script>
+
+<div
+	class="relative bg-[#213451] rounded-r-xl text-white text-base md:text-xl
+before:absolute before:h-full before:w-2 before:bg-[#9A69FF]"
+>
+	<p class="w-full lg:w-4/5 p-6">
+		{#each lines as line}
+			{line}
+			<br />
+		{/each}
+	</p>
+</div>
