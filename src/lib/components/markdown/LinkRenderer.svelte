48f4571187802e12298c67fb3162155f2a9e428e
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-10 17:21:16 +0530

    Added Link Renderer

---
+++ src/lib/components/markdown/LinkRenderer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let text: string;
+	export let href: string;
+
+	const target = href.startsWith('#') ? '_self' : '_blank';
+</script>
+
+<a
+	class="text-white text-base md:text-xl border-b-2 border-accent
+	hover:text-accent transition-colors ease-out duration-200 block my-2 w-fit"
+	{href}
+	{target}
+	rel="noopener noreferrer">{text}</a
+>
