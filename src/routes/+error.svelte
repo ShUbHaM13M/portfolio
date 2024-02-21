c583c2148e0e45abf0ab86523b3fe357266363f1
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-21 21:48:33 +0530

    Added some default styling to +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,8 +5,9 @@
 
 <Header />
 
-<main>
-	<div>
+<!-- TODO: Styling the error page -->
+<main class="min-h-screen w-full grid place-items-center">
+	<div class="flex flex-col gap-1 text-white">
 		<h1>Error Occured</h1>
 		<p>Seems like some unknown error happened</p>
 		<Button href="/">Let's start over</Button>
