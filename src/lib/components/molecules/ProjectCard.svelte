c10ce19213ebd1a7070108b7f03d1b2b92fda2f3
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-22 13:40:56 +0530

    Remove Tag from project card and work experience card

--- src/lib/components/molecules/ProjectCard.svelte
+++ src/lib/components/molecules/ProjectCard.svelte
@@ -8,51 +8,39 @@
 </script>
 
 <script lang="ts">
-	import Tag from '../atoms/Tag.svelte';
-
+	export let slug: string;
 	export let imageUrl: string | undefined = undefined;
 	export let name: string = '';
 	export let excerpt: string = '';
 	export let link: Link | undefined = undefined;
 
 	export let containerClasses: string | undefined = undefined;
-	export let tags: CustomTag[] = [];
 </script>
 
-<div
-	class={`container project-card flex flex-col gap-4 p-4 py-5 text-white rounded-lg bg-card transition-all ease-out duration-300 ${containerClasses}`}
->
-	{#if imageUrl}
-		<img
-			class="project-card-image aspect-video w-full rounded-md"
-			src={imageUrl}
-			alt={`Screenshot of ${name}`}
-		/>
-	{/if}
-	<div class="flex justify-between items-center">
-		<h3 class="font-medium text-2xl leading-6">{name}</h3>
-		{#if link}
-			<a href={link.href} target="_blank">
-				<svelte:component this={link.icon} classes="h-7 w-7" />
-			</a>
+<a href={`/${slug}`} class={`h-full block ${containerClasses}`}>
+	<div
+		class={`container project-card h-full flex flex-col gap-4 p-4 py-5 text-white rounded-lg bg-card transition-all ease-out duration-300 ${containerClasses}`}
+	>
+		{#if imageUrl}
+			<img
+				class="project-card-image aspect-video w-full rounded-md object-top"
+				src={imageUrl}
+				alt={`Screenshot of ${name}`}
+			/>
 		{/if}
-	</div>
-	<p class="text-lg leading-tight line-clamp-3">
-		{excerpt}
-	</p>
-	{#if tags}
-		<div class="flex flex-wrap gap-2">
-			{#each tags.slice(0, 3) as tag}
-				<Tag
-					label={tag.label}
-					text={tag.textColor}
-					color={tag.accentColor}
-					highlightOnHover={false}
-				/>
-			{/each}
+		<div class="flex justify-between items-center">
+			<h3 class="font-medium text-2xl leading-6">{name}</h3>
+			{#if link}
+				<a href={link.href} target="_blank">
+					<svelte:component this={link.icon} classes="h-7 w-7" />
+				</a>
+			{/if}
 		</div>
-	{/if}
-</div>
+		<p class="text-lg leading-tight line-clamp-3">
+			{excerpt}
+		</p>
+	</div>
+</a>
 
 <style lang="postcss" scoped>
 	.project-card {
--- src/lib/components/molecules/WorkExperienceCard.svelte
+++ src/lib/components/molecules/WorkExperienceCard.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
 	import type { WorkExperience } from '$lib/utils/types';
-	import Tag from '../atoms/Tag.svelte';
 
 	export let additionalClass: string = '';
 	export let work: WorkExperience;
 </script>
 
 <div
-	class={`flex flex-1 flex-col gap-2 md:gap-5 p-4 md:p-6 bg-[#343F52] text-white rounded-lg ${additionalClass}`}
+	class={`container flex flex-1 flex-col gap-4 p-4 py-5 bg-card text-white rounded-lg ${additionalClass}`}
 >
 	<div>
 		<p class="font-extrabold text-2xl md:text-3xl">{work.company}</p>
 		<p class="text-lg">{work.designation}</p>
 		<p class="text-base">{work.timeFrame}</p>
 	</div>
-	<p class="text-base md:text-xl">{work.description}</p>
+	<p class="text-lg leading-tight">{work.description}</p>
 
-	<div class="flex gap-1 md:gap-2 mt-4 md:mt-auto">
+	<!-- <div class="flex gap-1 md:gap-2 mt-4 md:mt-auto">
 		{#each work.tags as tag}
 			<Tag
 				color={tag.accentColor}
@@ -25,5 +24,5 @@
 				highlightOnHover={false}
 			/>
 		{/each}
-	</div>
+	</div> -->
 </div>
