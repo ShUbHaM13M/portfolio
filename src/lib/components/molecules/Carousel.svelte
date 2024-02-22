1e21eb99d3e7e021c60fcd4838b2649743935c05
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-22 13:43:46 +0530

    Added Carousel component

---
+++ src/lib/components/molecules/Carousel.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Image from '../atoms/Image.svelte';
+
+	export let images: string[] = [];
+</script>
+
+<div class="flex gap-4 size-72">
+	{#each images as image}
+		<Image src={image} alt="" additionalClasses="rounded-lg" />
+	{/each}
+</div>
