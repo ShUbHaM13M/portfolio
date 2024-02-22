da4858c448b3b685ed2e332fce7e18b57e89a8a4
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-02-22 13:41:40 +0530

    Added slug param to ProjectCards

--- src/lib/components/organism/Project.svelte
+++ src/lib/components/organism/Project.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import Section from '$lib/components/atoms/Section.svelte';
 	import ProjectCard from '$lib/components/molecules/ProjectCard.svelte';
-	import IconGithub from '$lib/icons/IconGithub.svelte';
-	import IconExternalLink from '$lib/icons/IconExternalLink.svelte';
 
+	// TODO: Rendering project links
 	import type { Project as ProjectType } from '$lib/utils/types';
 
-	// TODO: Replace projects with .md files
 	export let projects: ProjectType[] = [];
 </script>
 
@@ -19,16 +17,16 @@
 	<div class="project-container grid gap-4 mt-2 sm:mt-4">
 		{#each projects as project, index}
 			<ProjectCard
+				slug={`project/${project.slug}`}
 				name={project.name}
 				excerpt={project.description}
 				imageUrl={project?.images?.length ? project.images[0] : undefined}
-				tags={project.technologies}
 				containerClasses={`${index > 2 ? 'hidden md:flex' : ''}`}
 			/>
 		{/each}
 		<a
 			title="See All Projects"
-			href="/projects"
+			href="/project"
 			class="project-card mt-0 grid py-2 place-items-center w-full h-full
 			bg-accent bg-opacity-30 hover:bg-opacity-40 border-2 border-accent border-opacity-20 backdrop-blur-md
 			rounded-lg text-xl md:text-2xl font-medium text-white focus:outline-none focus-visible:outline-none
