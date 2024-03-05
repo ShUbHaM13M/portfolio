96e863831bbcbbf68d513a8b2a3d5a04a3474bb4
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-03-05 14:05:24 +0530

    Added sharing in projects

--- src/lib/icons/IconGithub.svelte
+++ src/lib/icons/IconGithub.svelte
@@ -8,7 +8,7 @@
 	viewBox="0 0 24 24"
 	fill="none"
 	xmlns="http://www.w3.org/2000/svg"
-	class={`h-12 w-12 ${classes}`}
+	class={`${classes}`}
 	{...$$restProps}
 >
 	<path
--- src/routes/(waves)/project/[slug=string]/+page.svelte
+++ src/routes/(waves)/project/[slug=string]/+page.svelte
@@ -1,13 +1,45 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { slide } from 'svelte/transition';
+
 	import Image from '$lib/components/atoms/Image.svelte';
 	import Tag from '$lib/components/atoms/Tag.svelte';
 	import Markdown from '$lib/components/molecules/Markdown.svelte';
 	import type { Project } from '$lib/utils/types';
 	import '$lib/themes/codeblock/frappe.css';
 	import IconGithub from '$lib/icons/IconGithub.svelte';
+	import IconShare from '$lib/icons/IconShare.svelte';
+	import { siteBaseUrl } from '$lib/data/meta';
 
 	export let data: { project: Project };
 	$: ({ project } = data);
+
+	let shareButton: HTMLButtonElement | null = null;
+	let canShare: boolean = false;
+	let shareData: ShareData | null = null;
+
+	async function onShareClick() {
+		await navigator.share(shareData!);
+	}
+
+	$: if (canShare) {
+		const timeout = setTimeout(() => {
+			shareButton?.addEventListener('click', onShareClick);
+		}, 510);
+		() => clearTimeout(timeout);
+	}
+
+	onMount(() => {
+		const shareableData = {
+			title: project.name,
+			text: project.description,
+			url: `${siteBaseUrl}/project/${project.slug}`
+		};
+		canShare = navigator.canShare(shareableData);
+		if (canShare) {
+			shareData = shareableData;
+		}
+	});
 </script>
 
 {#key project}
@@ -18,17 +50,41 @@
 		p-6 rounded-t-xl md:mx-auto md:w-11/12 md:rounded-xl h-full
 		backdrop-blur-md md:backdrop-blur-lg md:border md:border-white md:border-opacity-20"
 	>
-		<div class="flex flex-col sm:flex-row justify-between">
-			<h1 class="leading-tight md:leading-none text-[28px] md:text-[42px] font-bold">
+		<div class="flex flex-col sm:flex-row gap-2 justify-between overflow-hidden">
+			<h1 class="leading-tight md:leading-snug text-[28px] md:text-[42px] font-bold">
 				{project.name}
 			</h1>
-			{#if project.githubLink}
-				<a target="_blank" rel="noopener noreferrer" href={project.githubLink}>
-					<IconGithub classes="icon-link size-9 md:size-12 opacity-100" />
-				</a>
-			{/if}
+			<div class="flex gap-2 items-center w-full sm:w-fit">
+				{#if project.githubLink}
+					<a
+						transition:slide={{ axis: 'x', duration: 250 }}
+						target="_blank"
+						rel="noopener noreferrer"
+						class="block"
+						href={project.githubLink}
+					>
+						<IconGithub
+							classes="text-primary hover:text-accent-light dark:text-white dark:hover:text-accent transition-all ease-out duration-300 size-8 opacity-100"
+						/>
+					</a>
+				{/if}
+				{#if canShare}
+					<button
+						id="shareButton"
+						bind:this={shareButton}
+						transition:slide={{ axis: 'x', duration: 500 }}
+						type="button"
+						class="inline-block"
+						title={`Share ${project.name}`}
+					>
+						<IconShare
+							classes="text-primary hover:text-accent-light dark:text-white dark:hover:text-accent transition-all ease-out duration-300 size-8 opacity-100"
+						/>
+					</button>
+				{/if}
+			</div>
 		</div>
-		{#if project.technologies.length}
+		{#if project.technologies?.length}
 			<div class="flex flex-wrap gap-2">
 				{#each project.technologies as tag}
 					<Tag
@@ -42,7 +98,7 @@
 		{/if}
 
 		{#if project.images}
-			<div class="flex sm:flex-wrap gap-4 align-center overflow-x-auto w-full">
+			<div class="flex gap-4 align-center overflow-x-auto w-full">
 				{#each project.images as image, index}
 					<div class="image-container w-fit sm:w-80 h-fit sm:h-96 pb-4 sm:pb-0">
 						<!-- TODO: Add support for image alt -->
@@ -74,9 +130,10 @@
 	:global(.image-container img) {
 		max-height: 60svh;
 	}
+	/* TODO: Fix issues when rendering images in desktop */
 	@screen sm {
 		.image-container {
-			min-width: 0;
+			min-width: 40%;
 		}
 		:global(.image-container img) {
 			max-height: 100%;
