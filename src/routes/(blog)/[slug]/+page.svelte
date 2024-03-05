e4387d7f8a102775bfa93b3898a7e746bcc2c3ac
ShUbHaM13M <shubham.heeralal@gmail.com>
2024-03-05 14:11:39 +0530

    Added sharing option in blogs

--- src/routes/(blog)/[slug]/+page.svelte
+++ src/routes/(blog)/[slug]/+page.svelte
@@ -1,11 +1,43 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { slide } from 'svelte/transition';
+	import { siteBaseUrl } from '$lib/data/meta';
+
 	import Image from '$lib/components/atoms/Image.svelte';
 	import Tag from '$lib/components/atoms/Tag.svelte';
 	import BlogUserProfile from '$lib/components/molecules/BlogUserProfile.svelte';
 	import Markdown from '$lib/components/molecules/Markdown.svelte';
 	import type { Blog } from '$lib/utils/types';
+	import IconShare from '$lib/icons/IconShare.svelte';
 	export let data: { blog: Blog };
 	$: ({ blog } = data);
+
+	let shareButton: HTMLButtonElement | null = null;
+	let canShare: boolean = false;
+	let shareData: ShareData | null = null;
+
+	async function onShareClick() {
+		await navigator.share(shareData!);
+	}
+
+	$: if (canShare) {
+		const timeout = setTimeout(() => {
+			shareButton?.addEventListener('click', onShareClick);
+		}, 510);
+		() => clearTimeout(timeout);
+	}
+
+	onMount(() => {
+		const shareableData: ShareData = {
+			title: blog.title,
+			text: blog.excerpt,
+			url: `${siteBaseUrl}/blog/${blog.slug}`
+		};
+		canShare = navigator.canShare(shareableData);
+		if (canShare) {
+			shareData = shareableData;
+		}
+	});
 </script>
 
 {#key blog}
@@ -24,9 +56,27 @@
 		backdrop-blur-md md:backdrop-blur-lg md:border md:border-white md:border-opacity-20"
 	>
 		<BlogUserProfile date={blog.date.toDateString()} readTime={blog.readTime} />
-		<h1 class="leading-tight md:leading-none text-[28px] md:text-[42px] font-bold">
-			{blog.title}
-		</h1>
+
+		<div class="flex flex-col sm:flex-row gap-2 items-baseline justify-between overflow-hidden">
+			<h1 class="leading-tight md:leading-none text-[28px] md:text-[42px] font-bold">
+				{blog.title}
+			</h1>
+
+			{#if canShare}
+				<button
+					id="shareButton"
+					bind:this={shareButton}
+					transition:slide={{ axis: 'x', duration: 350 }}
+					type="button"
+					class="inline-block"
+					title={`Share ${blog.title}`}
+				>
+					<IconShare
+						classes="text-primary hover:text-accent-light dark:text-white dark:hover:text-accent transition-all ease-out duration-300 size-8 opacity-100"
+					/>
+				</button>
+			{/if}
+		</div>
 		{#if blog.tags.length}
 			<div class="flex flex-wrap gap-2">
 				{#each blog.tags as tag}
